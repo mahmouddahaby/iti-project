4bbc0b79b81badad19f2c0122903d26241f5b738
mahmouddahaby <mahmoud.dahaby7@gmail.com>
2022-09-26 13:45:12 +0200

    adding sub

--- hello.py
+++ hello.py
@@ -3,3 +3,11 @@ x = 10
 my_list = [1 , 3, 4, 5, 6]
 my_out= (filter(lambda num:num % 3  == 0, my_list)
     print(list(my_out))
+
+def my_sub(x, y)
+    x = int(input("x: "))
+    y = int(input("y: "))
+    z = x + y
+    return z
+
+print(my_sub)
